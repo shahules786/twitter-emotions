6a765a48d59e1adae5ee79e98dc46d75701e69a8
shahules786 <shahules786@gmail.com>
2021-03-14 12:26:53 +0530

    utils

--- twitteremotions/utils/config.py
+++ twitteremotions/utils/config.py
@@ -1,7 +1,10 @@
+
 class Config:
+    
     def __init__(self):
+        
 
         self.DEVICE = "cuda"
         self.MAX_LEN = 168
-        self.PATH = "../input/tf-roberta/"
+        self.PATH = '../input/tf-roberta/'
         self.MODEL_PATH = ""
