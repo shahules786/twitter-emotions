92f7be2ab55380756b6bc9222750d4b019e50d0c
shahules786 <shahules786@gmail.com>
2021-03-27 21:03:22 +0530

    update architecture

--- twitteremotions/utils/model.py
+++ twitteremotions/utils/model.py
@@ -12,15 +12,18 @@ def emotion_model(path="data/tf_roberta/", maxlen=168):
     bert_model = TFRobertaModel.from_pretrained(path + "pretrained-roberta-base.h5", config=config)
     x = bert_model(ids, attention_mask=att, token_type_ids=tok)
 
-    x1 = tf.keras.layers.Dropout(0.1)(x[0])
-    x1 = tf.keras.layers.Conv1D(1, 1)(x1)
+    # END INDEX HEAD
+    x2 = tf.keras.layers.Dropout(0.1)(x[0])
+    x2b = tf.keras.layers.Dense(1)(x2)
+    x2 = tf.keras.layers.Flatten()(x2b)
+    x2 = tf.keras.layers.Activation("softmax")(x2)
+
+    # START INDEX HEAD
+    x1 = tf.keras.layers.Concatenate()([x2b, x[0]])
+    x1 = tf.keras.layers.Dropout(0.1)(x1)
+    x1 = tf.keras.layers.Dense(1)(x1)
     x1 = tf.keras.layers.Flatten()(x1)
     x1 = tf.keras.layers.Activation("softmax")(x1)
-
-    x2 = tf.keras.layers.Dropout(0.1)(x[0])
-    x2 = tf.keras.layers.Conv1D(1, 1)(x2)
-    x2 = tf.keras.layers.Flatten()(x2)
-    x2 = tf.keras.layers.Activation("softmax")(x2)
 
     model = tf.keras.models.Model(inputs=[ids, att, tok], outputs=[x1, x2])
     optimizer = tf.keras.optimizers.Adam(learning_rate=3e-5)
