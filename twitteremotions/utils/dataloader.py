e9d6f6f3a85b2901959415ecd571f5c762f5d12d
Shahul ES <shahules@Shahuls-MacBook-Air.local>
2021-04-04 12:05:32 +0530

    dataloader

--- twitteremotions/utils/dataloader.py
+++ twitteremotions/utils/dataloader.py
@@ -1,6 +1,7 @@
 import tokenizers
 import numpy as np
-import tensorflow as tf
+import torch
+from torch.utils.data import Dataset
 
 
 class Dataprocess:
@@ -14,19 +15,21 @@ class Dataprocess:
 
     def preprocess_bert(self):
 
-        input_ids = np.ones((1, self.MAX_LEN), dtype="int32")
-        attention_mask = np.zeros((1, self.MAX_LEN), dtype="int32")
-        token_type_id = np.zeros((1, self.MAX_LEN), dtype="int32")
+        input_ids = np.ones((self.MAX_LEN), dtype="int32")
+        attention_mask = np.zeros((self.MAX_LEN), dtype="int32")
 
         assert type(self.text) == str, "input should be a string"
 
         self.text = " " + " ".join(self.text.split())
         enc = self.tokenizer.encode(self.text)
         sent_id = self.sentiment_id[self.sentiment]
-        input_ids[0, : len(enc.ids) + 5] = [0] + enc.ids + [2, 2] + [sent_id] + [2]
-        attention_mask[0, : len(enc.ids) + 5] = 1
+        input_ids[: len(enc.ids) + 5] = [0] + enc.ids + [2, 2] + [sent_id] + [2]
+        attention_mask[: len(enc.ids) + 5] = 1
 
-        return [input_ids, attention_mask, token_type_id]
+        return {
+            "input_ids": torch.tensor(input_ids, dtype=torch.long),
+            "attention_mask": torch.tensor(attention_mask, dtype=torch.long),
+        }
 
     def preprocess_output(self, pred_start, pred_end):
 
@@ -38,82 +41,60 @@ class Dataprocess:
         return output
 
 
-class DataGenerator(tf.keras.utils.Sequence):
+class EmotionData(Dataset):
 
     """generates data"""
 
-    def __init__(self, df, tokenizer, batch_size=32, shuffle=False, is_test=False, MAX_LEN=168):
+    def __init__(self, df, tokenizer, is_test=False, MAX_LEN=168):
 
         self.MAX_LEN = MAX_LEN
-        self.tokenizer = tokenizer
-        self.batch_size = batch_size
         self.df = df
         self.sentiment_id = {"positive": 1313, "negative": 2430, "neutral": 7974}
-        self.shuffle = shuffle
         self.is_test = is_test
         self.list_IDs = df.index.values.tolist()
-        self.on_epoch_end()
+        self.tokenizer = tokenizer
 
     def __len__(self):
         "Denotes the number of batches per epoch"
-        return len(self.list_IDs) // self.batch_size
+        return len(self.df)
 
     def __getitem__(self, index):
-        "Generate one batch of data"
-        # Generate indexes of the batch
-        indexes = self.indexes[index * self.batch_size : (index + 1) * self.batch_size]
 
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        input_ids = np.ones((self.MAX_LEN), dtype="int32")
+        attention_mask = np.zeros((self.MAX_LEN), dtype="int32")
 
-        # Generate data
+        start_tokens = np.zeros((self.MAX_LEN), dtype="int32")
+        end_tokens = np.zeros((self.MAX_LEN), dtype="int32")
 
-        return self.__data_generation(list_IDs_temp)
+        text1 = " " + " ".join(self.df.loc[index, "text"].split())
 
-    def on_epoch_end(self):
-        "Updates indexes after each epoch"
-        self.indexes = np.arange(len(self.list_IDs))
-        if self.shuffle:
-            np.random.shuffle(self.indexes)
+        enc = self.tokenizer.encode(text1)
 
-    def __data_generation(self, list_IDs_temp):
-        "Generates data containing batch_size samples"  # X : (n_samples, *dim, n_channels)
-        # Initialization
-        input_ids = np.ones((self.batch_size, self.MAX_LEN), dtype="int32")
-        attention_mask = np.zeros((self.batch_size, self.MAX_LEN), dtype="int32")
-        token_type_ids = np.zeros((self.batch_size, self.MAX_LEN), dtype="int32")
-        start_tokens = np.zeros((self.batch_size, self.MAX_LEN), dtype="int32")
-        end_tokens = np.zeros((self.batch_size, self.MAX_LEN), dtype="int32")
+        s_tok = self.sentiment_id[self.df.loc[index, "sentiment"]]
+        input_ids[: len(enc.ids) + 5] = [0] + enc.ids + [2, 2] + [s_tok] + [2]
+        attention_mask[: len(enc.ids) + 5] = 1
 
-        # Generate data
-        for k, ID in enumerate(list_IDs_temp):
-            # Store sample
-            # FIND OVERLAP
-            text1 = " " + " ".join(self.df.loc[ID, "text"].split())
-            enc = self.tokenizer.encode(text1)
+        if not self.is_test:
 
-            s_tok = self.sentiment_id[self.df.loc[ID, "sentiment"]]
-            input_ids[k, : len(enc.ids) + 5] = [0] + enc.ids + [2, 2] + [s_tok] + [2]
-            attention_mask[k, : len(enc.ids) + 5] = 1
+            text2 = " ".join(self.df.loc[index, "selected_text"].split())
+            idx = text1.find(text2)
+            chars = np.zeros((len(text1)))
+            chars[idx : idx + len(text2)] = 1
+            if text1[idx - 1] == " ":
+                chars[idx - 1] = 1
+            offsets = enc.offsets
+            toks = []
+            for i, (a, b) in enumerate(offsets):
+                sm = np.sum(chars[a:b])
+                if sm > 0:
+                    toks.append(i)
+            if len(toks) > 0:
+                start_tokens[toks[0] + 1] = 1
+                end_tokens[toks[-1] + 1] = 1
 
-            if not self.is_test:
-                text2 = " ".join(self.df.loc[ID, "selected_text"].split())
-                idx = text1.find(text2)
-                chars = np.zeros((len(text1)))
-                chars[idx : idx + len(text2)] = 1
-                if text1[idx - 1] == " ":
-                    chars[idx - 1] = 1
-                offsets = enc.offsets
-                toks = []
-                for i, (a, b) in enumerate(offsets):
-                    sm = np.sum(chars[a:b])
-                    if sm > 0:
-                        toks.append(i)
-                if len(toks) > 0:
-                    start_tokens[k, toks[0] + 1] = 1
-                    end_tokens[k, toks[-1] + 1] = 1
-
-                return ((input_ids, attention_mask, token_type_ids), (start_tokens, end_tokens))
-
-            else:
-                return ((input_ids, attention_mask, token_type_ids),)
+        return {
+            "input_ids": torch.tensor(input_ids, dtype=torch.long),
+            "attention_mask": torch.tensor(attention_mask, dtype=torch.long),
+            "start_tokens": torch.tensor(start_tokens, dtype=torch.long),
+            "end_tokens": torch.tensor(end_tokens, dtype=torch.long),
+        }
